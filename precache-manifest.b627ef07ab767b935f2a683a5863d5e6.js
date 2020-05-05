d0ea010f04cc6d9932dfd5c900451b62975bea97
Akash singh <robinakash123@gmail.com>
2020-05-05 23:46:05 +0530

    Updates

---
+++ precache-manifest.b627ef07ab767b935f2a683a5863d5e6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f84917859f5189f3990ded09270252",
+    "url": "/logo-designer/index.html"
+  },
+  {
+    "revision": "77d5624b1c350f6ebe17",
+    "url": "/logo-designer/static/css/2.901522a5.chunk.css"
+  },
+  {
+    "revision": "77fadfc511feebc8fa74",
+    "url": "/logo-designer/static/css/main.11205016.chunk.css"
+  },
+  {
+    "revision": "77d5624b1c350f6ebe17",
+    "url": "/logo-designer/static/js/2.04808d33.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/logo-designer/static/js/2.04808d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fadfc511feebc8fa74",
+    "url": "/logo-designer/static/js/main.587c6be1.chunk.js"
+  },
+  {
+    "revision": "8d7f7fb105a5983c4e7d",
+    "url": "/logo-designer/static/js/runtime-main.cc2065af.js"
+  },
+  {
+    "revision": "cd27c89b44196084d6a529eedb5c9587",
+    "url": "/logo-designer/static/media/about-us-bg.cd27c89b.jpg"
+  },
+  {
+    "revision": "c6a2c0ff59f26eba5ef593cee07da2fc",
+    "url": "/logo-designer/static/media/graphic.c6a2c0ff.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/logo-designer/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/logo-designer/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/logo-designer/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/logo-designer/static/media/slick.f97e3bbf.svg"
+  }
+]);
